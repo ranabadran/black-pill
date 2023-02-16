f1784a5cf3d0747fca39c9c4c5670d734f915a35
ranabadran <ranab456@gmail.com>
2023-02-16 13:15:09 +0200

    Initial commit of Blinking LED

---
+++ include/gpio.h
@@ -0,0 +1,19 @@
+/**
+  ******************************************************************************
+  * @file    gpio.h
+  * @brief   This file contains all the function prototypes for
+  *          the gpio.c file
+  * @author	 Rana B.
+  ******************************************************************************
+  */
+
+#ifndef GPIO_H_
+#define GPIO_H_
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* functions prototypes ---------------------------------------------*/
+void GPIO_Initialization(void);
+
+#endif /* GPIO_H_ */
---
+++ include/main.h
@@ -0,0 +1,20 @@
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  * @author			: Rana B.
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Exported functions prototypes ---------------------------------------------*/
+void delay(unsigned int n);
+
+#endif /* MAIN_H_ */
